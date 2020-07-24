b380933f4de6e7305addd8f43c9473328a99001e
FranekWawa <68611842+FranekWawa@users.noreply.github.com>
2020-07-24 19:34:31 +0200

    Every 5th shape the previous shapes change color

--- Python/random_polygons.py
+++ Python/random_polygons.py
@@ -2,6 +2,7 @@ from graphics import *
 import random
 import math
 
+
 def polyPoints(sides,radius, x_centre,y_centre):
     angle = math.radians(0)
     angle_increment = math.radians(360/sides)
@@ -14,28 +15,53 @@ def polyPoints(sides,radius, x_centre,y_centre):
         points.append(Point(x,y))
     return points
         
-def Randcolor():
+def randColor():
     r = random.randint(0,255)
     g = random.randint(0,255)    
     b = random.randint(0,255)
     return color_rgb(r,g,b)
+
 def main():
     win = GraphWin("My Circle", 500, 500)
     win.setBackground('blue')
+    
+    #iterations
+    i=0
+    #store objects
+    shapes = []
+    
     while True:
-        number = random.randint(2,10)
+        #count iterations
+        i+=1
+        
+        #get cursor position 
         Cursor = win.getMouse()
-        color = Randcolor()
         x = Cursor.getX()
         y = Cursor.getY()
+        
+        #random color
+        color = randColor()
+
+        #random shapes
+        number = random.randint(2,10)
         if number == 2:
             s = Circle(Cursor, 20)
         if number > 2:
             points = polyPoints(number, 20,x,y)
             s = Polygon(points)
+            
+        #drawing and saving shapes
         s.setFill(color)
-        
         s.draw(win)
+        shapes.append(s)
+
+        #change color of previous objects
+        if i == 5:
+            for shape in shapes[:-1]:
+                shape.undraw()
+                shape.setFill(randColor())
+                shape.draw(win)
+            i = 0    
     
 
 main()
